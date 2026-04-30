07e5dc13beae9a240b0330620c80b3c9ca5990ed
Vasyl Kyslianka <vasyl.kyslianka@lnu.edu.ua>
2026-04-30 17:33:55 +0300

    CRUD-interface for administrator

--- inventory-app/src/components/ConfirmModal.jsx
+++ inventory-app/src/components/ConfirmModal.jsx
@@ -10,4 +10,4 @@ export default function ConfirmModal({ onConfirm, onCancel, message }) {
       </div>
     </div>
   );
-}
+} // hmm
--- inventory-app/src/pages/AdminInventory.jsx
+++ inventory-app/src/pages/AdminInventory.jsx
@@ -88,4 +88,4 @@ export default function AdminInventory() {
       )}
     </div>
   );
-}
+} //hmm
